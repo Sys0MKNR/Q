39e5305fb6af9ef2bf43c084921e5accad71f3e8
Sys0MKNR <michael.szecsoedi@gmail.com>
2023-07-01 19:43:21 +0200

    fix import error

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,61 @@
-const path = require('path')
-const util = require('util')
+const path = require("path");
+const util = require("util");
 
-const { src, dest, parallel, series } = require('gulp')
-const pug = require('gulp-pug')
-const sass = require('gulp-sass')(require('sass'))
-const minifyCSS = require('gulp-csso')
-const uglify = require('gulp-terser')
-const gulpCopy = require('gulp-copy')
-const del = require('del')
+const { src, dest, parallel, series } = require("gulp");
+const pug = require("gulp-pug");
+const sass = require("gulp-sass")(require("sass"));
+const minifyCSS = require("gulp-csso");
+const uglify = require("gulp-terser");
+const gulpCopy = require("gulp-copy");
+const del = require("del");
 
 // const runPKG = require('./build/pkg.js')
 
-const BUILD_DIR = '.build'
-const BUILD_TMP = path.join(BUILD_DIR, '.tmp')
-const DIST_DIR = 'dist'
+const BUILD_DIR = ".build";
+const BUILD_TMP = path.join(BUILD_DIR, ".tmp");
+const DIST_DIR = "dist";
 
-const BUILD_APP_DIR = path.join(BUILD_DIR, 'app')
-const BUILD_PUBLIC_DIR = path.join(BUILD_APP_DIR, 'public')
+const BUILD_APP_DIR = path.join(BUILD_DIR, "app");
+const BUILD_PUBLIC_DIR = path.join(BUILD_APP_DIR, "public");
 
-async function clean () {
-  await del([
-    BUILD_TMP,
-    DIST_DIR
-  ])
+async function clean() {
+  await del([BUILD_TMP, DIST_DIR]);
 }
 
-async function pkg () {
+async function pkg() {
   // await runPKG()
 }
 
-function shared () {
-  return src('src/shared/**/*')
-    .pipe(gulpCopy(BUILD_APP_DIR, { prefix: 1 }))
+function server() {
+  return src("src/server/**/*").pipe(gulpCopy(BUILD_APP_DIR, { prefix: 2 }));
 }
 
-function server () {
-  return src('src/server/**/*')
-    .pipe(gulpCopy(BUILD_APP_DIR, { prefix: 2 }))
-}
-
-function html () {
-  return src('src/client/pug/*.pug')
+function html() {
+  return src("src/client/pug/*.pug")
     .pipe(pug())
-    .pipe(dest(path.join(BUILD_PUBLIC_DIR, 'html')))
+    .pipe(dest(path.join(BUILD_PUBLIC_DIR, "html")));
 }
 
-function css () {
-  return src('src/client/sass/style.sass')
-    .pipe(sass().on('error', sass.logError))
+function css() {
+  return src("src/client/sass/style.sass")
+    .pipe(sass().on("error", sass.logError))
     .pipe(minifyCSS())
-    .pipe(dest(path.join(BUILD_PUBLIC_DIR, 'css')))
+    .pipe(dest(path.join(BUILD_PUBLIC_DIR, "css")));
 }
 
-function js () {
-  return src('src/client/js/*.js', { sourcemaps: true })
+function js() {
+  return src("src/client/js/*.js", { sourcemaps: true })
     .pipe(uglify())
-    .pipe(dest(path.join(BUILD_PUBLIC_DIR, 'js'), { sourcemaps: true }))
+    .pipe(dest(path.join(BUILD_PUBLIC_DIR, "js"), { sourcemaps: true }));
 }
 
-function font () {
-  return src('src/client/font/*')
-    .pipe(gulpCopy(path.join(BUILD_PUBLIC_DIR, 'font'), { prefix: 3 }))
+function font() {
+  return src("src/client/font/*").pipe(
+    gulpCopy(path.join(BUILD_PUBLIC_DIR, "font"), { prefix: 3 })
+  );
 }
 
-exports.clean = clean
-exports.client = parallel(html, css, js, font)
-exports.build = series(clean, shared, server, exports.client)
-exports.default = series(exports.build, pkg)
+exports.clean = clean;
+exports.client = parallel(html, css, js, font);
+exports.build = series(clean, server, exports.client);
+exports.default = series(exports.build, pkg);
--- src/server/route/api.js
+++ src/server/route/api.js
@@ -1,63 +1,51 @@
-const UTIL = require('../util')
+const UTIL = require("../util");
 // const { Packet } = require('../data')
 
-const { WSPacket, ImgPacket, WSError } = require('../shared/dto')
+const { WSPacket, ImgPacket, WSError } = require("../dto");
 
-module.exports = function route (fastify, opts, next) {
-  fastify.addHook('preHandler', (req, reply, next) => {
+module.exports = function route(fastify, opts, next) {
+  fastify.addHook("preHandler", (req, reply, next) => {
     if (!req.$validUser) {
-      reply.unauthorized()
+      reply.unauthorized();
     }
-    next()
-  })
+    next();
+  });
 
+  fastify.post("/exit", async (req, reply) => {
+    reply.send("ok");
+    process.exit();
+  });
 
-  fastify.post('/exit', async (req, reply) => {
-    reply
-      .send('ok')
-    process.exit()
-  })
-
-  fastify.get('/ws', { websocket: true }, (conn, req) => {
-
-    conn.socket.on('message', async msg => {
+  fastify.get("/ws", { websocket: true }, (conn, req) => {
+    conn.socket.on("message", async (msg) => {
       try {
-        const packet = new WSPacket(JSON.parse(msg))
-        fastify.$server.log.info(packet)
+        const packet = new WSPacket(JSON.parse(msg));
+        fastify.$server.log.info(packet);
 
-      
-        if (packet.type in WSPacket.TYPE){
-          handleWS[packet.type](fastify, conn, req, packet)
+        if (packet.type in WSPacket.TYPE) {
+          handleWS[packet.type](fastify, conn, req, packet);
         } else {
-          conn.socket.send(JSON.stringify(new WSError('bad msg type')))
+          conn.socket.send(JSON.stringify(new WSError("bad msg type")));
         }
-
-
       } catch (error) {
-        conn.socket.send(JSON.stringify(new WSError('bad msg')))
-        console.error(error)
+        conn.socket.send(JSON.stringify(new WSError("bad msg")));
+        console.error(error);
       }
-    })
+    });
+  });
 
-  })
-
-  next()
-}
+  next();
+};
 
 const handleWS = {
+  async IMG(fastify, conn, req, packet) {
+    const payload = packet.payload;
 
-  async IMG (fastify, conn, req, packet) {
-    const payload = packet.payload
-
-    if(payload.type === ImgPacket.TYPE.START){
-      fastify.$server.sc.addClient(conn, req)
-      
-    } else if(payload.type === ImgPacket.TYPE.STOP){    
-      const id = req.session.get('SCClientID')
-      fastify.$server.sc.removeClient(id)
-
+    if (payload.type === ImgPacket.TYPE.START) {
+      fastify.$server.sc.addClient(conn, req);
+    } else if (payload.type === ImgPacket.TYPE.STOP) {
+      const id = req.session.get("SCClientID");
+      fastify.$server.sc.removeClient(id);
     }
-    
-
-  }
-}
+  },
+};
